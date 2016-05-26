07085c57f1e282f8a36e4138ef1bb53781438870
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-05-26 14:55:39 -0400

    Updated from Brython Server: 5/26/2016 2:55:39 PM

--- monstergame.py
+++ monstergame.py
@@ -1,4 +1,5 @@
-openingStory = input("""You are in a network of caves filled with monsters, locked doors, and one final boss. 
+openingStory = input("""You are in a network of caves filled with monsters, locked doors, and one final boss.
+
 Your job is to make it to the room with the final boss, which contains the door to the outside world and freedom. 
 But beware, the boss is a very challenging opponent who should not be taken lightly.
 If you try to attack it or any other monsters with a weapon that is not strong enough, the monster will take your weapon and kill you.
@@ -9,10 +10,18 @@ In some rooms there are key which can be used to open locked doors or weapons to
 Good Luck!""")
 
 bluePossesion = 0
+goldPossesion = 0
+bronzeSword = 0
+steelSword = 0
+
 if openingStory == "N":
     openingStory = input("There is a solid stone wall. You cannot go North.")
 if openingStory == "S":
     weaponRoom1 = input("""You travel through a tunnel and eventually come to a room with a chest in one corner. 
     The chest is banded in gold and has a large padlock holding it closed. Fortunately, the padlock is haging open,
     unlocked. Press O to open the box.""")
-    #if weaponRoom1 == 
+    if weaponRoom1 == "O":
+        openChest = input("  
+if openingStory == "E":
+    emptyRoom1 == 
+    
