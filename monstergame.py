532ab4482b1c3ccb799fa6e98c298b5698dc2fd2
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-06-10 02:41:58 -0400

    Updated from Brython Server: 6/10/2016 2:41:57 AM

--- monstergame.py
+++ monstergame.py
@@ -12,7 +12,7 @@ while not endKey:
     
     if roomNumber == 1:
         exitRoom = False
-        y = input(bluePossession)
+        print(bluePossession)
         if bluePossesion:
             question = "You are in a room with an empty pedestal"
         else:
