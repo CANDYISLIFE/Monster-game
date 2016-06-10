01f1d34b7b41f5c8dcf680a78fe2d25155dd8ed7
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-06-10 01:07:32 -0400

    Updated from Brython Server: 6/10/2016 1:07:31 AM

--- monstergame.py
+++ monstergame.py
@@ -46,9 +46,18 @@ while not endKey:
                 
     if roomNumber == 2:
         exitRoom = False
-        question = """You travel through a tunnel and eventually come to a room with a chest in one corner. 
-        The chest is banded in gold and has a large padlock holding it closed. Fortunately, the padlock is haging open,
-        unlocked. Press O to open the box."""
+        question = """You are in a network of caves filled with monsters, locked doors, and one final boss.
+    You are armed with only the clothes on your back, a small folding knife, and your wits.
+    The room you are currently in has tunnel entrances in the south, east and west.
+    Your job is to make it to the room with the final boss, which contains the door to the outside world and freedom. 
+    But beware, the boss is a very challenging opponent who should not be taken lightly.
+    If you try to attack it or any other monsters with a weapon that is not strong enough, the monster will take your weapon and kill you.
+    If you type in N, S, E, or W, you will leave the room in the direction you type. 
+    In some rooms you cannot go in every direction because there is a wall or locked door. 
+    The boss room is in the south-east corner of the map. 
+    In some rooms there are key which can be used to open locked doors or weapons to kill monsters. 
+    Press Q to quit.
+    Good Luck!"""
         while not exitRoom:
             validInput = False
             while not validInput:
